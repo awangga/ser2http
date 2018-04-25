d266f441a5953b4d24b55d236477efbc84336b7b
Rolly Maulana Awangga <rolly@awang.ga>
2018-04-25 14:25:58 +0700

    add some

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import webbrowser
 from lib.cilok import urlEncode16
 from lib import config
 
-port="/dev/cu.wch ch341 USB=>RS232 3d10"
-baudrate = 115200
 
-ser = serial.Serial(port, baudrate)
+ser = serial.Serial(config.port, config.baudrate)
 temp = ''
 while 1:
 	data=ser.readline().rstrip('\n')
 	#print data
 	data=data.strip()
 	print data
-	if data[:1]=="0":
+	if data[:1]=="[":
 		print "\a"
 		trimdata = data.replace(" ","")
 		thedata = urlEncode16(trimdata)
