2fced9347af0c3dc8049ab4e27e33f25ca4c1b3d
Rolly Maulana Awangga <rolly@awang.ga>
2019-04-01 09:53:39 +0700

    add main

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import serial
-import webbrowser
-from lib.cilok import urlEncode16
 from lib import config
 
 
 ser = serial.Serial(config.port, config.baudrate)
-temp = ''
+
 while 1:
-	data=ser.readline().rstrip('\n')
+	#data=ser.readline().rstrip('\n')
+	data=ser.readline()
 	#print data
-	data=data.strip()
-	print data
-	if data[:1]=="[":
-		print "\a"
-		trimdata = data.replace(" ","")
-		thedata = urlEncode16(trimdata)
-		uri = config.keyuri+'%input%ktp%'+trimdata
-		thedata = urlEncode16(uri)
-		webbrowser.open_new(config.host+thedata)
+	#data=data.strip()
+	print(data)
+	#if data[:1]=="[":
+	#	print "\a"
+	#	trimdata = data.replace(" ","")
+	#	thedata = urlEncode16(trimdata)
+	#	uri = config.keyuri+'%input%ktp%'+trimdata
+	#	thedata = urlEncode16(uri)
+	#	webbrowser.open_new(config.host+thedata)
 
